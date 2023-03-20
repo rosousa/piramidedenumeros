cc95dde235fe28791f7997c7628fcb1326a6aa97
rosousa <rodolfozsousa@gmail.com>
2023-03-20 19:51:13 -0300

    add user input and repeated number table

--- src/PiramideDeNumeros.java
+++ src/PiramideDeNumeros.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PiramideDeNumeros {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Digite um número!");
 
+        int number = userInput.nextInt();
+
+        for(int i = 1; i <= number; i++) {
+            for(int k = 1; k <= i; k++) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
     }
 }
